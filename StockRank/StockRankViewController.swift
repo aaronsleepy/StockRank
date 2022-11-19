fc0ca21528373e311f7f26f093d59145a8c20b2b
aaronsleepy <aaron@kmong.com>
2022-11-19 14:53:56 +0900

    ui: Collection View 추가 및 Cell 기본 구조 설정

--- StockRank/StockRankViewController.swift
+++ StockRank/StockRankViewController.swift
@@ -9,9 +9,12 @@ import UIKit
 
 class StockRankViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+         
     }
 
 
