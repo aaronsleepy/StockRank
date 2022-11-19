027cd534044dfed715c2bd247073823e6b120aaa
aaronsleepy <aaron@kmong.com>
2022-11-19 15:56:13 +0900

    feat: UICollectionView, UICollectionViewDelegate extension

--- StockRank/StockRankViewController.swift
+++ StockRank/StockRankViewController.swift
@@ -8,15 +8,31 @@
 import UIKit
 
 class StockRankViewController: UIViewController {
+    // 사용할 Data
+    let stocks: [StockModel] = StockModel.list
 
     @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-         
+        // data 알려주기
+        collectionView.dataSource = self
+        // layout 알려주기
+        collectionView.delegate = self
     }
-
-
 }
 
+extension StockRankViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return stocks.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+}
+
+extension StockRankViewController: UICollectionViewDelegateFlowLayout {
+    
+}
